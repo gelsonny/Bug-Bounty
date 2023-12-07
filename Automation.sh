3790cc6aa62d9123f45888e4b8ac486a87724601
gelsonny <51079512+gelsonny@users.noreply.github.com>
2023-12-07 17:00:24 +0200

    Update Automation.sh

--- Automation.sh
+++ Automation.sh
@@ -1,3 +1,4 @@
+cat automation.sh                                                   
 #!/bin/bash
 
 #1- crt.sh
@@ -8,7 +9,8 @@
 #6-getallurls
 #7-gospider
 #8-katana
-
+#9-getJS
+#10-waybackurl
 
 #first step...make a directory and a file with the name of the target
 mkdir /home/kali/BugBounty/$1
@@ -27,17 +29,19 @@ subfinder -d $1 -o /home/kali/BugBounty/$1/subfinder-$1.txt
 #Ok
 
 cat /home/kali/BugBounty/$1/assetfinder-$1.txt >> /home/kali/BugBounty/$1/$1.txt
-cat /home/kali/BugBounty/$1/sublist-$1.txt >> /home/kali/BugBounty/$1/$1.txt 
+cat /home/kali/BugBounty/$1/sublist3r-$1.txt >> /home/kali/BugBounty/$1/$1.txt 
 cat /home/kali/BugBounty/$1/subfinder-$1.txt >> /home/kali/BugBounty/$1/$1.txt
 cat /home/kali/BugBounty/crt.sh/output/$1.txt >> /home/kali/BugBounty/$1/$1.txt
 sort /home/kali/BugBounty/$1/$1.txt | uniq > /home/kali/BugBounty/$1/SortedDomains-$1.txt
 cat /home/kali/BugBounty/$1/SortedDomains-$1.txt | httpx-toolkit > /home/kali/BugBounty/$1/alive-$1
+httpx-toolkit -l /home/kali/BugBounty/$1/$1.txt  -p 80,443,8080,3000,9090 -status-code -title -o /home/kali/BugBounty/$1/httpx-status-code.txt
+
 
 ##Cariddi##
-cat /home/kali/BugBounty/$1/alive-$1 | cariddi -e -s -rua > /home/kali/BugBounty/$1/cariddi-$1.txt
+cat /home/kali/BugBounty/$1/alive-$1 | cariddi -e -s -rua > /home/kali/BugBounty/$1/cariddi.txt
 
 ##getAllUrls##
-cat /home/kali/BugBounty/$1/alive-$1 | getallurls -o /home/kali/BugBounty/$1/getAllUrls-$1.txt
+cat /home/kali/BugBounty/$1/alive-$1 | getallurls -o /home/kali/BugBounty/$1/getAllUrls.txt
 
 ##gospider##
 mkdir /home/kali/BugBounty/$1/gospidy
@@ -46,15 +50,26 @@ touch /home/kali/BugBounty/$1/finalresults.txt
 gospider -S /home/kali/BugBounty/$1/alive-$1 --sitemap -a -v -d 3 -o /home/kali/BugBounty/$1/gospidy/.
 
 ##katana##
-katana -u /home/kali/BugBounty/$1/alive-$1 -jc -o /home/kali/BugBounty/$1/katana-output
+katana -u /home/kali/BugBounty/$1/alive-$1 -jc -o /home/kali/BugBounty/$1/katana
+
+##GetJS##
+getJS --input /home/kali/BugBounty/$1/$1-alive --complete > /home/kali/BugBounty/$1/getjs
+
+##waybackurls##
+cat /home/kali/BugBounty/$1/$1-alive | waybackurls > /home/kali/BugBounty/$1/waybackurl
 
 
 
-cat /home/kali/BugBounty/$1/cariddi-$1.txt >> finalresults.txt
-cat /home/kali/BugBounty/$1/getAllUrls-$1.txt >> finalresults.txt
-cat /home/kali/BugBounty/$1/gospidy/* >> finalresults.txt
-cat /home/kali/BugBounty/$1/katana >> finalresult.txt'
-cat cariddi-$1.txt | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u > /home/kali/BugBounty/$1/cariddi-out.txt
+sort /home/kali/BugBounty/$1/cariddi-$1.txt | uniq >> finalresults.txt
+sort /home/kali/BugBounty/$1/getAllUrls-$1.txt | uniq >> finalresults.txt
+sort /home/kali/BugBounty/$1/gospidy/* | uniq  >> finalresults.txt
+sort /home/kali/BugBounty/$1/katana | uniq >> finalresult.txt
+sort /home/kali/BugBounty/$1/waybackurl | uniq >> finalresult.txt
+
+cat finalresult.txt | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u > /home/kali/BugBounty/$1/final-output.txt
 
 
 
+#to use AllForOne nuclei >> exposure scripts and jS scripts:
+#find -name '*-exposure-*' -exec cp "{}" ./exposure/  \;
+#https://github.com/arkadiyt/bounty-targets-data/blob/main/data/wildcards.txt
